098bd5804fc5c2e4fe9b159c6ae0b7bb7ff14b95
Jonas Helfer <jonas@helfer.email>
2017-06-22 21:11:15 -0700

    polish initial template

--- data/schema.js
+++ data/schema.js
@@ -2,19 +2,16 @@ import {
   makeExecutableSchema,
   addMockFunctionsToSchema,
 } from 'graphql-tools';
+import mocks from './mocks'
 
-import mocks from './mocks';
-
-const typeDefinitions = `
+const typeDefs = `
 type Query {
   testString: String
 }
-
-schema {
-  query: Query
-}
 `;
 
-const schema = makeExecutableSchema({ typeDefs: typeDefinitions });
+const schema = makeExecutableSchema({ typeDefs });
+
 addMockFunctionsToSchema({ schema, mocks });
+
 export default schema;
