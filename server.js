503f12ea8ef0c0107841cb06139de2c35031b47c
Jonas Helfer <jonas@helfer.email>
2017-06-27 12:05:25 -0700

    Update server.js to print better startup msg

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
-  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
+  `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
 ));
