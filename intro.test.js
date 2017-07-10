875d7518184a52172d2bfc87d4793f104c64c2f3
Angello Pozo <Angello.pozo@gmail.com>
2017-07-10 12:17:03 -0700

    added real tests.

--- intro.test.js
+++ intro.test.js
@@ -1,24 +1,16 @@
 
 const { buildSchema, graphql } = require('graphql');
-
-const temporary2 = `
-type Value {
-  string: String
-  int: Int
-}
-type candy {
-  value: Value
-}
-type Query {
-  candy: candy
-}
+const { queryTypes, allResolvers } = require('./lib/createQLType')
+const createFnQueryLines = require('./lib/createFnQueryLines')
+const schemaSource = `
+${queryTypes}
+${createFnQueryLines}
 `
-const schema = buildSchema(temporary2)
 
-const getCandy = require('./lib/methods/getCandy')
+const schema = buildSchema(schemaSource)
+const rootValue = allResolvers
 
-
-it('should query a public uint value', async () => {
+it('should succesfully query a public uint value', async () => {
   const query = `
     query {
       candy {
@@ -28,17 +20,82 @@ it('should query a public uint value', async () => {
         }
       }
     }
-  `;
-
-  const rootValue = {
-    candy: () => {
-      return getCandy()
-    }
-  };
-
+  `
   const result = await graphql(schema, query, rootValue);
-  // const { data } = result;
-  console.log(result)
+  expect(result.data).toEqual({'candy':{'value':{'string':'6','int':6}}})
+})
 
-  // expect(data.viewer.me).toBe(null);
-});
+it('should succesfully query a source string value', async () => {
+  const query = `
+    query {
+      source {
+    		string
+      }
+    }
+  `
+  const result = await graphql(schema, query, rootValue);
+  expect(result.data).toEqual({ 'source': { 'string': 'source' } })
+})
+
+it('should succesfully query getBalance', async () => {
+  const query = `
+  query {
+    getBalance(addr: "0x7b2c6c6e9026bcef8df4df3ff888b72b018f0e8a") {
+      value {
+        string
+        int
+      }
+    }
+  }
+  `
+  const result = await graphql(schema, query, rootValue);
+  expect(result.data).toEqual({'getBalance':{'value':{'string':'0','int':0}}})
+})
+
+it('should succesfully query getBalanceInEth', async () => {
+  const query = `
+  query {
+    getBalanceInEth(addr: "0x7b2c6c6e9026bcef8df4df3ff888b72b018f0e8a") {
+      value {
+        string
+        int
+      }
+    }
+  }
+  `
+  const result = await graphql(schema, query, rootValue);
+  expect(result.data).toEqual({'getBalanceInEth':{'value':{'string':'0','int':0}}})
+})
+
+it('should succesfully query other with multiple outputs', async () => {
+  const query = `
+  query {
+    other {
+  		string
+      bytes32
+      value {
+        string
+        int
+      }
+    }
+  }
+  `
+  const result = await graphql(schema, query, rootValue);
+  expect(result.data).toEqual({'other':{'string':'hey','bytes32':'0x0000000000000000000000000000000000000000000000000000000000000011','value':{'string':'600','int':600}}})
+})
+
+it('should succesfully query returns2 with multiple inputs/outputs', async () => {
+  const query = `
+  query {
+    returns2(addr: "0x7b2c6c6e9026bcef8df4df3ff888b72b018f0e8d" num: 1) {
+  		boolean
+      value {
+        string
+        int
+      }
+    }
+  }
+  `
+  const result = await graphql(schema, query, rootValue);
+  expect(result.data).toEqual({'returns2':{'boolean':true,'value':{'string':'10000','int':10000}}})
+})
